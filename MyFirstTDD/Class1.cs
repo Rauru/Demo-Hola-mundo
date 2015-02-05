30ce8f8c06a3d3f125fa3aad57ecbc7c7e1515e0
Rauru <raul_lopez@unitec.edu>
2015-02-05 17:34:59 -0600

    first commit

---
+++ MyFirstTDD/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using StringUtilities;
+
+namespace MyFirstTDD
+{
+    [Subject(typeof(StringUtilties))]
+    public class when_concatenating_two_strings
+    {
+        private static StringUtilties sut;
+        private static string input1;
+        private static string input2;
+        private static string actualvalue;
+
+        Establish context = () =>
+        {
+            sut = new StringUtilties();
+            input1 = "Hello";
+            input2 = "World";
+        };
+        private Because of = () =>
+        {
+            actualvalue = sut.Concatenate(input1, input2);
+        };
+        private It should_concatenate_both_input_strings = () =>
+        {
+            actualvalue.ShouldEqual("HelloWorld"); 
+        };
+        
+    }
+    
+
+    
+}
---
+++ StringUtilities/StringUtilties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringUtilities
+{
+    public class StringUtilties
+    {
+        public string Concatenate(string input1, string input2)
+        {
+            return input1 + input2;
+        }
+    }
+}
